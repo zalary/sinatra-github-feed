4cc6c6d309b25e03a3dcdd5ba4ff2d41083fd39a
Zalary Young <zalary@gmail.com>
2014-02-21 13:33:05 -0600

    added lib

---
+++ lib/github.rb
@@ -0,0 +1,21 @@
+require 'unirest'
+require 'pry'
+
+module GHFeed
+  class RepoInfo
+    def self.get_repo_data(username, repo)
+      url = "https://api.github.com/repos/#{username}/#{repo}/commits"
+      response = Unirest.get url, auth:{ :user=>ENV["PASSWORD"], :password=>"x-oauth-basic" }
+      response.body
+    end
+  end
+  class UserInfo
+    def self.gravatar_url(username)
+    url = "https://api.github.com/users/#{username}"
+    response = Unirest.get url, auth:{ :user=>ENV["PASSWORD"], :password=>"x-oauth-basic"] }
+    response.body
+    grav_id = response.body['gravatar_id']
+    gravatar_url = "https://secure.gravatar.com/avatar/#{grav_id}"
+    end
+  end
+end
