1ccca0ec75666baeef150bb94cf82ac23fb92e12
Zalary Young <zalary@gmail.com>
2014-02-21 14:35:34 -0600

    updated to use gh app token

--- lib/github.rb
+++ lib/github.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'unirest'
 require 'pry'
 
@@ -5,14 +6,15 @@ module GHFeed
   class RepoInfo
     def self.get_repo_data(username, repo)
       url = "https://api.github.com/repos/#{username}/#{repo}/commits"
-      response = Unirest.get url, auth:{ :user=>ENV["PASSWORD"], :password=>"x-oauth-basic" }
+      response = Unirest.get url, auth:{ :user=>ENV["PASSWORD"]}
       response.body
+    # binding.pry
     end
   end
   class UserInfo
     def self.gravatar_url(username)
     url = "https://api.github.com/users/#{username}"
-    response = Unirest.get url, auth:{ :user=>ENV["PASSWORD"], :password=>"x-oauth-basic"] }
+    response = Unirest.get url, auth:{ :user=>ENV["PASSWORD"]}
     response.body
     grav_id = response.body['gravatar_id']
     gravatar_url = "https://secure.gravatar.com/avatar/#{grav_id}"
